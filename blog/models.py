7cbcd9de2624a4f8f1b75543e8a598d67cdc1c77
rvandorp <ryan@MacBook-Air.local>
2018-10-27 12:10:34 -0400

    Completed Phase 1 of personal site, including jobs and blog

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,14 @@ class Blog(models.Model):
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
 
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
 
 # 1. Create blog model (per above)
 
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,10 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static/')
+]
+
+# run python manage.py collectstatic
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
 
 
